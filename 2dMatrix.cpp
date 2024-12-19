b7abdd3a34865d154f33c92885374b9320f714d6
Irfan <151149459+Nobita02xx@users.noreply.github.com>
2024-12-19 20:56:45 +0530

    Add files via upload

---
+++ 2dMatrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+addition(int arr[2][2],int arr1[2][2]){
+    int add[2][2];
+    for (int i = 0; i < 2; i++)
+    { 
+        for (int j = 0; j < 2; j++)
+        {
+            add[i][j]=arr[i][j]+arr1[i][j];
+        }   
+    }
+    return add[][]
+}
+int main(){
+    int arr[2][2],arr1[2][2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
+    }
+    
+  for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin>>arr1[i][j];
+        }
+        
+    }
+    
+}
+      
